fac05f4b71c6a1965b3b9c9b509762da5c20e1d1
郭桓桓 <hi@nekohuan.cyou>
2022-05-21 18:33:45 +0800

    fix: performance problem & correct vars

--- req/js/actions.js
+++ req/js/actions.js
@@ -5,34 +5,37 @@
 
 /* DOM 載入完畢 */
 $(document).ready(() => {
-    // 打字特效
-    let i = 0;
-    const txtelmnt = $("main > p");
-    const cntnt = txtelmnt.text();
-    txtelmnt.empty();
-    function typWritr() {
-        if (i < cntnt.length) {
-            txtelmnt.append(cntnt.charAt(i));
-            i++; setTimeout(typWritr, 100);
-        }
-    }
-    typWritr();
+    // jQuery 正規化
+    const $ = jQuery.noConflict();
+    typeWriter();
     // 分頁失焦
-    const orginlTitl = document.title;
+    const originalTitle = document.title;
     $(function() {
         $(window).focus(function() {
-            document.title = orginlTitl;
+            document.title = originalTitle;
         });
         $(window).blur(function() {
             document.title = "人怎麼不見了╭(°A°`)╮";
         });
-    });
+    };
     // 頁腳年份
     if (new Date().getYear() !== 121) {
         $("#footerYear").html("-" + new Date().getFullYear().toString() + "&nbsp;");
-    }
+    };
     // 禁止右鍵
     $("html").on("contextmenu", function() {
         return false;
     });
+    // 打字特效
+    let i = 0;
+    const intro = $("main > p");
+    const introContent = intro.text();
+    intro.empty();
+    function typeWriter() {
+        if (i < introContent.length) {
+            intro.append(introContent.charAt(i));
+            i++;
+	    setTimeout(typeWriter, 100);
+        };
+    };
 });
